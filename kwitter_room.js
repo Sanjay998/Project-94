aa39fd832e045ec27ae202863616f42278757e59
Sanjay998 <79947433+Sanjay998@users.noreply.github.com>
2021-12-15 16:55:17 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+//import { initializeApp } from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDnIcOPHHUFwhC0yQ9Nvk4V8zYVj5JzqmE",
+  authDomain: "fairy-school-55df2.firebaseapp.com",
+  databaseURL: "https://fairy-school-55df2-default-rtdb.firebaseio.com",
+  projectId: "fairy-school-55df2",
+  storageBucket: "fairy-school-55df2.appspot.com",
+  messagingSenderId: "750527114130",
+  appId: "1:750527114130:web:477693c7f7bd9de1a26f4b"
+};
+
+
+firebase.initializeApp(firebaseConfig);
